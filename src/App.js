ba518cd17225a5d3a10a297d00730acb6968e33c
Bilal Zak <bzak954@gmail.com>
2021-07-27 13:33:40 +0000

    add components and routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import React from "react";
+import Home from './Pages/Home'
+import About from './Pages/About'
+import Contact from './Pages/Contact'
+import Services from './Pages/Services'
+import ServicesDetail from './Pages/ServicesDetail'
+import './nav.css'
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+      <div className="nav">
+          <Link to="/"><button>Home</button></Link>
+          <Link to="/about"><button>About</button></Link>
+          <Link to="/contact"><button>Contact</button></Link>
+          <Link to="/services"><button>Services</button></Link>
+      </div>
+      
+          <Route path="/" exact component={Home}/>
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/services" exact component={Services}/>
+          <Route path="/services/:id" component={ServicesDetail}/>
+    </Router>
+    </>
   );
 }
 
