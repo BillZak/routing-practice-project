8162c0199a9beeb90c4dd310463a0f1772da7d0f
Bilal Zak <bzak954@gmail.com>
2021-07-27 13:35:46 +0000

    add service id link

---
+++ src/Pages/ServicesDetail.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Services = (props) => {
+    return (
+        <div>
+            <h1>Services Detail Page[{props.match.params.id}]</h1>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+             Aliquid fugit eveniet deserunt, accusamus atque ex beatae 
+             eos recusandae perspiciatis cum?
+        </div>
+    );
+}
+
+export default Services;
