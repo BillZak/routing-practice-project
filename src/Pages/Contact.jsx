f7ff2635291979f661fed155f6b22cf78f137f00
Bilal Zak <bzak954@gmail.com>
2021-07-27 13:34:56 +0000

    add contact link

---
+++ src/Pages/Contact.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Contact = () => {
+    return (
+        <div>
+            <h1>Contact Page</h1>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+             Aliquid fugit eveniet deserunt, accusamus atque ex beatae 
+             eos recusandae perspiciatis cum?
+        </div>
+    );
+}
+
+export default Contact;
