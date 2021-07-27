c622b783accd916e5afbf80ca3385837a909fa72
Bilal Zak <bzak954@gmail.com>
2021-07-27 13:35:10 +0000

    add home routed link

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Home Page</h1>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+             Aliquid fugit eveniet deserunt, accusamus atque ex beatae 
+             eos recusandae perspiciatis cum?
+        </div>
+    );
+}
+
+export default Home;
