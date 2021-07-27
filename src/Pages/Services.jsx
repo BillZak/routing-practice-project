086bc004cf0de71255791673c475d919f2f276a1
Bilal Zak <bzak954@gmail.com>
2021-07-27 13:35:26 +0000

    add services routed link

---
+++ src/Pages/Services.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Services = () => {
+    return (
+        <div>
+            <h1>Services Page</h1>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+             Aliquid fugit eveniet deserunt, accusamus atque ex beatae 
+             eos recusandae perspiciatis cum?
+        </div>
+    );
+}
+
+export default Services;
