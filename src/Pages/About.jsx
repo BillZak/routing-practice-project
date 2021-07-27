7aeaedc1eb7c55dcf1c92a9172b4af8b9bf76e1e
Bilal Zak <bzak954@gmail.com>
2021-07-27 13:34:37 +0000

    add nav link

---
+++ src/Pages/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h1>About Page</h1>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+             Aliquid fugit eveniet deserunt, accusamus atque ex beatae 
+             eos recusandae perspiciatis cum?
+        </div>
+    );
+}
+
+export default About;
