d357feec5e5251fd30edc4405704829394183e01
Bilal Zak <bzak954@gmail.com>
2021-07-28 18:45:23 +0000

    add text

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
             <h1>About Page</h1>
             Lorem ipsum dolor sit amet consectetur adipisicing elit.
              Aliquid fugit eveniet deserunt, accusamus atque ex beatae 
-             eos recusandae perspiciatis cum?
+             eos recusandaes perspiciatis cum?
         </div>
     );
 }
