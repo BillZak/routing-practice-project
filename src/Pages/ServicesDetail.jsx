b2bb9d21461859dad6e93bfe0c67b996db685f3f
Bilal Zak <bzak954@gmail.com>
2021-07-28 18:45:52 +0000

    add text

--- src/Pages/ServicesDetail.jsx
+++ src/Pages/ServicesDetail.jsx
@@ -6,7 +6,7 @@ const Services = (props) => {
             <h1>Services Detail Page[{props.match.params.id}]</h1>
             Lorem ipsum dolor sit amet consectetur adipisicing elit.
              Aliquid fugit eveniet deserunt, accusamus atque ex beatae 
-             eos recusandae perspiciatis cum?
+             eos recusandaes perspiciatis cum?
         </div>
     );
 }
