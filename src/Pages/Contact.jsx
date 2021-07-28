4ed29cb956f446ad465d6117fb2a021dbb097d23
Bilal Zak <bzak954@gmail.com>
2021-07-28 18:45:29 +0000

    add text

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -6,7 +6,7 @@ const Contact = () => {
             <h1>Contact Page</h1>
             Lorem ipsum dolor sit amet consectetur adipisicing elit.
              Aliquid fugit eveniet deserunt, accusamus atque ex beatae 
-             eos recusandae perspiciatis cum?
+             eos recusandaes perspiciatis cum?
         </div>
     );
 }
